31f81879f9261643668752b6fff989ff8f5a7a8f
ketan.kulkarni <ketan@amrutraj.com>
2018-12-21 00:07:13 +0530

    basic done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputText: "",
+      data: []
+    };
+  }
+
+  handleInput = e => {
+    this.setState({ inputText: e.target.value });
+  };
+
+  handleAdd = e => {
+    e.preventDefault();
+    const { data, inputText } = this.state;
+    if (inputText.trim().length === 0) {
+      return alert("enter task name");
+    }
+    data.push({ id: new Date(), task: inputText, isComplete: false });
+    this.setState({ data, inputText: "" });
+  };
+
+  status = () => {
+    const { data } = this.state;
+    const totalTasks = data.length;
+    const status = data.reduce(
+      (acc, cur) => {
+        if (cur.isComplete) {
+          acc.complete++;
+        } else acc.incomplete++;
+        return acc;
+      },
+      { complete: 0, incomplete: 0 }
+    );
+    return totalTasks ? (
+      <p>{`${status.incomplete} remains out of ${totalTasks}`}</p>
+    ) : (
+      "enter task"
+    );
+  };
+
+  markComplete = s => {
+    const { data } = this.state;
+    const index = data.findIndex(item => item.id === s.id);
+    data[index].isComplete = true;
+    this.setState({ data });
+  };
+
   render() {
+    const { inputText, data } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form onSubmit={this.handleAdd}>
+          <input onChange={this.handleInput} value={inputText} />
+          <button style={{ marginLeft: "10px" }} onClick={this.handleAdd}>
+            add
+          </button>
+        </form>
+        {this.status()}
+        <ul>
+          {data.map(item => (
+            <li
+              key={item.id}
+              onClick={() => this.markComplete(item)}
+              style={{ textDecoration: item.isComplete ? "line-through" : "" }}
+            >
+              {item.task}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
